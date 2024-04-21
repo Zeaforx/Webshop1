9aece765e5f1bbb35b9437c0690cd593deb4401e
Zeaforx <ajibcaleb@gmail.com>
2024-04-21 11:43:46 -0700

    last

--- app/[ProductDetails]/page.tsx
+++ app/[ProductDetails]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 
-import PayPalButton from "@/components/PayPalButton";
+// import PayPalButton from "@/components/PayPalButton";
 import { useRouter } from "next/navigation";
 import { useSearchParams } from "next/navigation";
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
