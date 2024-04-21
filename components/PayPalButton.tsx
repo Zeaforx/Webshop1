f86f9235f1bb8384ce834098abddf1babe9c1c2c
Zeaforx <ajibcaleb@gmail.com>
2024-04-21 11:39:02 -0700

    buttojj

--- components/PayPalButton.tsx
+++ components/PayPalButton.tsx
@@ -1,33 +1,32 @@
-import { PayPalButtons, PayPalScriptProvider } from "@paypal/react-paypal-js";
-const PayPalButton = () => {
-    const createOrder = (data, actions) => {
-        return actions.order.create({
-            purchase_units: [
-                {
-                    amount: {
-                        value: "10.00", // Set the amount to the price of your product
-                    },
-                },
-            ],
-        });
-    };
+// import { PayPalButtons, PayPalScriptProvider } from "@paypal/react-paypal-js";
+// const PayPalButton = () => {
+//     const createOrder = (data: any, actions: { order: { create: (arg0: { purchase_units: { amount: { value: string; }; }[]; }) => any; }; }) => {
+//         return actions.order.create({
+//             purchase_units: [
+//                 {
+//                     amount: {
+//                         value: "10.00", // Set the amount to the price of your product
+//                     },
+//                 },
+//             ],
+//         });
+//     };
 
-    const onApprove = (data, actions) => {
-        return actions.order.capture().then(function (details) {
-            // Handle successful payment
-            alert("Transaction completed by " + details.payer.name.given_name);
-        });
-    };
+//     const onApprove = async (data: any, actions: { order: { capture: () => Promise<any>; }; }) => {
+//         const details = await actions.order.capture();
+//         // Handle successful payment
+//         alert("Transaction completed by " + details.payer.name.given_name);
+//     };
 
-    return (
-        <PayPalScriptProvider
-            options={{
-                "client-id": "YOUR_PAYPAL_CLIENT_ID",
-            }}
-        >
-            <PayPalButtons createOrder={createOrder} onApprove={onApprove} />
-        </PayPalScriptProvider>
-    );
-};
+//     return (
+//         <PayPalScriptProvider
+//             options={{
+//                 "clientId": "YOUR_PAYPAL_CLIENT_ID",
+//             }}
+//         >
+//             <PayPalButtons createOrder={createOrder} onApprove={onApprove} />
+//         </PayPalScriptProvider>
+//     );
+// };
 
-export default PayPalButton;
+// export default PayPalButton;
