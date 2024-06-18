86c207b30fd7d90224b368f47ef5542e354824ac
Zeaforx <ajibcaleb@gmail.com>
2024-06-18 23:51:30 +1300

    updatea4

--- components/form.tsx
+++ components/form.tsx
@@ -288,6 +288,7 @@ export default function Form(props) {
                             <PayPalButtons
                                 createOrder={(data, actions) => {
                                     return actions.order.create({
+                                        intent: "CAPTURE",
                                         purchase_units: [
                                             {
                                                 amount: {
